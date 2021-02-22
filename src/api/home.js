b8267eed931d0a139a48dd8f528e472304d9780c
huangmingyi <huangmingyi@iboxpay.com>
2021-02-22 14:47:03 +0800

    + 代理地址修改

--- src/api/home.js
+++ src/api/home.js
@@ -1,9 +1,9 @@
 const api = {
   login1:{ // 使用验证码或密码进行登陆
-    url: `/nf_user_auth_web/ignore_tk/uc/v1/seller_login.json`,
+    url: `/v1/low_websocket/send_message`,
     method: 'post'
   },
-  dds2:  '/nf_user_auth_web1/ignore_tk/uc/v1/seller_login.json'
+  dds2:  '/v1/low_websocket/chat_data'
 }
 
 export default api
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ const vitePluginRouterComponent = require("./vite-plugin-router-component")
 
 const path = require('path')
 
-/**
- * @type {import('vite').UserConfig}
- */
 export default {
+  server: {
+    proxy: { // 接口地址代理转换
+      '^/v1': {
+        target: 'https://haidj.top',
+        changeOrigin: true,
+      }
+    }
+  },
+
   alias: {
     // 键必须以斜线开始和结束
     '@': path.resolve(__dirname, './src')
